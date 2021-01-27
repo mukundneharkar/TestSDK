1bc6a5fe00f59b3bc1a038252450a6dc1a87432c
Mukund Neharkar <mukund.neharkar@logicmonitor.com>
2021-01-27 13:57:00 +0530

    build errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # extensions = ['rinoh.frontend.sphinx', 'sphinx.ext.autodoc', 'sphinx_markdown_builder']
-extensions = ['sphinx.ext.napoleon', 'rst2pdf.pdfbuilder', 'sphinx_markdown_builder',
-              'sphinxcontrib.confluencebuilder','recommonmark']
+extensions = ['sphinx.ext.napoleon', 'rst2pdf.pdfbuilder'
+              ,'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
